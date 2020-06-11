265a3d625c9140c67f2a251ba259210e6da3f916
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2020-06-11 10:23:25 +0200

    download precompiled llvm build for x86_64 linux

---
+++ cmake/InstallLLVM.cmake
@@ -0,0 +1,35 @@
+# This will install the LLVM dependency for FIRESTARTER
+# Usage:
+#   include(cmake/InstallLLVM.cmake)
+#   install_llvm()
+include(ExternalProject)
+
+	if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
+		#exec_program("${PROJECT_SOURCE_DIR}/cmake/downloadLLVM.sh https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz b25f592a0c00686f03e3b7db68ca6dc87418f681f4ead4df4745a01d9be63843" OUTPUT_VARIABLE RELATIVE_LLVM_DIR)
+		#SET(ENV{LLVM_DIR} ${PROJECT_SOURCE_DIR}/cmake/${RELATIVE_LLVM_DIR})
+		#find_package(LLVM REQUIRED CONFIG)
+
+		ExternalProject_Add(LLVMInstall
+			PREFIX ${PROJECT_SOURCE_DIR}/lib/LLVM
+			DOWNLOAD_DIR ${PROJECT_SOURCE_DIR}/lib/LLVM/download
+			SOURCE_DIR ${PROJECT_SOURCE_DIR}/lib/LLVM/sources
+			URL https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+			URL_HASH SHA256=b25f592a0c00686f03e3b7db68ca6dc87418f681f4ead4df4745a01d9be63843
+			CONFIGURE_COMMAND ""
+			BUILD_COMMAND ""
+			INSTALL_COMMAND ""
+		)
+
+		include_directories(${PROJECT_SOURCE_DIR}/lib/LLVM/sources/include)
+		add_library(LLVMSupport STATIC IMPORTED)
+		set_target_properties(LLVMSupport PROPERTIES IMPORTED_LOCATION ${PROJECT_SOURCE_DIR}/lib/LLVM/sources/lib/libLLVMSupport.a)
+
+	#SET(ENV{LLVM_DIR} ${PROJECT_SOURCE_DIR}/lib/LLVM/sources)
+	#	find_package(LLVM REQUIRED CONFIG)
+
+	#	include_directories(${LLVM_INCLUDE_DIRS})
+	#	add_definitions(${LLVM_DEFINITIONS})
+
+	elseif(CMAKE_SYSTEM_NAME STREQUAL "Darwin")
+	elseif(CMAKE_SYSTEM_NAME STREQUAL "Windows")
+	endif()
