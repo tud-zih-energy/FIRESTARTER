1aab8674afcea8ad3a648d914514dcb71323d252
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2020-12-07 22:20:30 +0100

    fix version string generation

--- src/firestarter/Main.cpp
+++ src/firestarter/Main.cpp
@@ -30,7 +30,6 @@
 int print_copyright() {
 
   firestarter::log::info()
-      << "\n"
       << "This program is free software: you can redistribute it and/or "
          "modify\n"
       << "it under the terms of the GNU General Public License as published "
@@ -48,7 +47,6 @@ int print_copyright() {
 int print_warranty() {
 
   firestarter::log::info()
-      << "\n"
       << "This program is distributed in the hope that it will be useful,\n"
       << "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
       << "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
