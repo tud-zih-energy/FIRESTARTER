4ae98e3ed14a42686cc9225aabb225941ec687f7
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2024-10-25 17:36:45 +0200

    clang-tidy: launch processes in new python thread

--- tooling/clang-tidy.py
+++ tooling/clang-tidy.py
@@ -9,6 +9,8 @@ import multiprocessing
 import sys
 import typing
 import random
+from multiprocessing import Pool
+from functools import partial
 
 # Find all source and header files in the project root that belong to FIRESTARTER
 def find_source_and_header_files(project_root: Path) -> typing.List[ Path ]:
@@ -26,6 +28,21 @@ def split_in_chunks(chunk_size: int, input: typing.List[Path]) -> typing.List[ty
         length += 1
     
     return [ input[i:i+length] for i in range(0, len(input), length)]
+
+# Run clang-tidy on a set of input files and return the stdout
+def run_clang_tidy(files: typing.List[Path], project_root_path: Path, build_root_path: Path, clang_tidy_file_path: Path) -> bytes:
+    command_args = ['clang-tidy', '-extra-arg=-std=c++17', f'-p={build_root_path}', f'--config-file={clang_tidy_file_path}', '--header-filter=include/firestarter/*', '--format-style=file']
+    command_args += files
+    print(f'Starting {command_args}')
+    p = subprocess.Popen(command_args, stdout=subprocess.PIPE, cwd=project_root_path)
+
+    # Wait for clang-tidy instances to terminate
+    if p.poll() is None:
+        p.wait()
+        stdout, _ = p.communicate()
+        return stdout + b'\n'
+    
+    return b''
 
 @click.command()
 @click.option('--project-root', default=Path(__file__).parent.parent.absolute(), help='The folder where the git repository is located.')
@@ -58,25 +75,13 @@ def clang_tidy_report(project_root, build_root, cores):
     files_scrambled = files.copy()
     random.shuffle(files_scrambled)
 
-    processes = set()
-    for chunck in split_in_chunks(cores, files_scrambled):
-        command_args = ['clang-tidy', '-extra-arg=-std=c++17', f'-p={build_root_path}', f'--config-file={clang_tidy_file_path}', '--header-filter=include/firestarter/*', '--format-style=file']
-        command_args += chunck
-        print(f'Starting {command_args}')
-        processes.add(subprocess.Popen(' '.join(command_args), shell=True, stdout=subprocess.PIPE, cwd=project_root_path))
-
-    # Wait for clang-tidy instances to terminate
-    complete_stdout = b''
-    for p in processes:
-        if p.poll() is None:
-            p.wait()
-            stdout, _ = p.communicate()
-            complete_stdout += stdout + b'\n'
+    with Pool(cores) as p:
+        stdout = p.map(partial(run_clang_tidy, project_root_path=project_root_path, build_root_path=build_root_path, clang_tidy_file_path=clang_tidy_file_path), split_in_chunks(cores, files_scrambled))
 
     clang_tidy_report_file = build_root_path / Path('clang-tidy-report.txt')
     print(f'Writing report to {clang_tidy_report_file}')
     with open(clang_tidy_report_file, 'wb') as fp:
-        fp.write(complete_stdout)
+        fp.write(b''.join(stdout))
 
 if __name__ == '__main__':
     clang_tidy_report()
