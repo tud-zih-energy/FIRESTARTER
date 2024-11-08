08a3772c992e96521d0c86d3407b2def1c58c3e8
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2024-11-08 15:35:13 +0100

    add docstring for config

--- include/firestarter/Config.hpp
+++ include/firestarter/Config.hpp
@@ -27,55 +27,97 @@
 
 namespace firestarter {
 
+/// This struct contains the parsed config from the command line for Firestarter.
 struct Config {
+  /// The argument vector from the command line.
   const char** Argv;
 
+  /// The timeout after which firestarter terminates. This is available in combination with optimization.
   std::chrono::seconds Timeout{};
+  /// The period after with which the low/high load routine is switched.
   std::chrono::microseconds Period{};
+  /// The load in the range of 0 < Load <= Period, which controls how long of the period the high-load loop runs.
   std::chrono::microseconds Load{};
 
+  /// The interval every which the register will be dumped to the file.
   std::chrono::seconds DumpRegistersTimeDelta = std::chrono::seconds(0);
+  /// The time to skip from the measurement start
   std::chrono::milliseconds StartDelta = std::chrono::milliseconds(0);
+  /// The time to skip from the measurement stop
   std::chrono::milliseconds StopDelta = std::chrono::milliseconds(0);
+  /// Metric values will be polled by the MeasurementInterval.
   std::chrono::milliseconds MeasurementInterval = std::chrono::milliseconds(0);
+  /// The time how long the processor will be preheated before starting a measurement or optimization.
   std::chrono::seconds Preheat{};
+  /// The time how long a measurement should take.
   std::chrono::seconds EvaluationDuration{};
 
+  /// The crossover probability used in the NSGA2 optimization algorithm.
   double Nsga2Cr;
+  /// The mutation probability used in the NSGA2 optimization algorithm.
   double Nsga2M;
 
+  /// The name of the metrics that are read from stdin.
   std::vector<std::string> StdinMetrics;
+  /// The paths to the metrics that are loaded using shared libraries.
   std::vector<std::string> MetricPaths;
+  /// The list of metrics that are used for maximization. If a metric is prefixed with '-' it will be minimized.
   std::vector<std::string> OptimizationMetrics;
 
+  /// The optional cpu bind that allow pinning to specific cpus.
   std::string CpuBind;
+  /// The optional selected instruction groups. If this is empty the default will be choosen.
   std::string InstructionGroups;
+  /// The file where the dump register feature will safe its output to.
   std::string DumpRegistersOutpath;
+  /// The name of the optimization algorithm.
   std::string OptimizationAlgorithm;
+  /// The file where the data saved during optimization is saved.
   std::string OptimizeOutfile;
 
+  /// The argument count from the command line.
   int Argc;
+  /// The requested number of threads firestarter should run with. 0 means all threads.
   unsigned RequestedNumThreads;
+  /// The selected function id. 0 means automatic selection.
   unsigned FunctionId;
+  /// The line count of the payload. 0 means default.
   unsigned LineCount = 0;
+  /// The number of gpus firestarter should stress. Default is -1 means all gpus.
   int Gpus = 0;
+  /// The matrix size which should be used. 0 means automatic detections.
   unsigned GpuMatrixSize = 0;
+  /// The number of individuals that should be used for the optimization.
   unsigned Individuals;
+  /// The number of generations that should be used for the optimization.
   unsigned Generations;
 
+  /// If the function summary should be printed.
   bool PrintFunctionSummary;
+  /// If the available instruction groups for a function should be printed.
   bool ListInstructionGroups;
+  /// Allow payloads that are not supported on the current processor.
   bool AllowUnavailablePayload = false;
+  /// Is the dump registers debug feature enabled?
   bool DumpRegisters = false;
+  /// Is the error detection feature enabled?
   bool ErrorDetection = false;
+  /// Should the GPUs use floating point precision? If neither GpuUseFloat or GpuUseDouble is set, precision will be
+  /// choosen automatically.
   bool GpuUseFloat = false;
+  /// Should the GPUs use double point precision? If neither GpuUseFloat or GpuUseDouble is set, precision will be
+  /// choosen automatically.
   bool GpuUseDouble = false;
+  /// Should we print all available metrics.
   bool ListMetrics = false;
+  /// Do we perform an measurement.
   bool Measurement = false;
+  /// Do we perform optimization.
   bool Optimize = false;
 
   Config() = delete;
 
+  /// Parser the config from the command line argumens.
   Config(int Argc, const char** Argv);
 };
 
