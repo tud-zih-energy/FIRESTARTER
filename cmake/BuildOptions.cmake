d04125df6244f937054dc66d1cb2c90af2b45a54
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2024-11-26 17:58:37 +0100

    cmake: do not use dependent options as they do not work for our intended usecase

--- cmake/BuildOptions.cmake
+++ cmake/BuildOptions.cmake
@@ -5,8 +5,9 @@ set(FIRESTARTER_BUILD_TYPE "FIRESTARTER" CACHE STRING "FIRESTARTER_BUILD_TYPE ca
 set_property(CACHE FIRESTARTER_BUILD_TYPE PROPERTY STRINGS FIRESTARTER FIRESTARTER_CUDA FIRESTARTER_ONEAPI FIRESTARTER_HIP)
 
 # Static linking is not supported with GPU devices or MacOS.
-set(FIRESTARTER_LINK_STATIC_FLAG ("${FIRESTARTER_BUILD_TYPE}" STREQUAL "FIRESTARTER") AND (NOT CMAKE_SYSTEM_NAME STREQUAL "Darwin"))
-cmake_dependent_option(FIRESTARTER_LINK_STATIC "Link FIRESTARTER as a static binary. Note, dlopen is not supported in static binaries. This option is not available on macOS or with CUDA, OneAPI or HIP enabled." ON "FIRESTARTER_LINK_STATIC_FLAG" OFF)
+if(${FIRESTARTER_BUILD_TYPE} STREQUAL "FIRESTARTER" AND NOT ${CMAKE_SYSTEM_NAME} STREQUAL "Darwin")
+	set(FIRESTARTER_LINK_STATIC "Link FIRESTARTER as a static binary. Note, dlopen is not supported in static binaries. This option is not available on macOS or with CUDA, OneAPI or HIP enabled." ON)
+endif()
 
 
 # We vendor hwloc per default.
@@ -14,10 +15,12 @@ option(FIRESTARTER_BUILD_HWLOC "Build hwloc dependency." ON)
 
 
 # Use of thread affinity is enabled on linux per default.
-set(FIRESTARTER_THREAD_AFFINITY_FLAG (CMAKE_SYSTEM_NAME STREQUAL "Linux"))
-cmake_dependent_option(FIRESTARTER_THREAD_AFFINITY "Enable FIRESTARTER to set affinity to hardware threads." ON "FIRESTARTER_THREAD_AFFINITY_FLAG" OFF)
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+	set(FIRESTARTER_THREAD_AFFINITY "Enable FIRESTARTER to set affinity to hardware threads." ON)
+endif()
 
 
 # Debug feature are enabled on linux per default.
-set(FIRESTARTER_DEBUG_FEATURES_FLAG (CMAKE_SYSTEM_NAME STREQUAL "Linux"))
-cmake_dependent_option(FIRESTARTER_DEBUG_FEATURES "Enable debug features" ON "FIRESTARTER_DEBUG_FEATURES_FLAG" OFF)
+if(${CMAKE_SYSTEM_NAME} STREQUAL "Linux")
+	set(FIRESTARTER_DEBUG_FEATURES "Enable debug features" ON)
+endif()
