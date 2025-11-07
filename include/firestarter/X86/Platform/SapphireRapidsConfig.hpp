3ea958534e29d565f15e984f4f137a68af6d7e0c
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-11-07 14:20:15 +0100

    Add Sapphire Rapids config stub

---
+++ include/firestarter/X86/Platform/SapphireRapidsConfig.hpp
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * FIRESTARTER - A Processor Stress Test Utility
+ * Copyright (C) 2024 TU Dresden, Center for Information Services and High
+ * Performance Computing
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/\>.
+ *
+ * Contact: daniel.hackenberg@tu-dresden.de
+ *****************************************************************************/
+
+#pragma once
+
+#include "firestarter/X86/Payload/AVX512Payload.hpp"
+#include "firestarter/X86/Platform/X86PlatformConfig.hpp"
+
+namespace firestarter::x86::platform {
+class SapphireRapidsConfig final : public X86PlatformConfig {
+public:
+  SapphireRapidsConfig() noexcept
+      : X86PlatformConfig(/*Name=*/"SPR_XEONEP", /*RequestedModels=*/
+                          {X86CpuModel(/*FamilyId=*/6, /*ModelId=*/143)},
+                          /*Settings=*/
+                          firestarter::payload::PayloadSettings(
+                              /*Threads=*/{1, 2},
+                              // 48KiB L1 per core
+                              // 2MiB L2 per core
+                              // 1.875MiB L3 per core
+                              /*DataCacheBufferSize=*/{49152, 2097152, 1966080},
+                              /*RamBufferSize=*/1048576000, /*Lines=*/1536,
+                              /*Groups=*/
+                              InstructionGroups{{{"RAM_L", 1}, {"L3_L", 1}, {"L2_L", 1}, {"L1_L", 1}, {"REG", 1}}}),
+                          /*Payload=*/std::make_shared<const payload::AVX512Payload>()) {}
+};
+} // namespace firestarter::x86::platform
