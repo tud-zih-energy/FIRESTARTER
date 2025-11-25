b0341edebb6fd387bab69871bf642416da57efdb
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-11-25 21:02:34 +0100

    SSE2Payload: return the number of iterations and implement max number of iterations logic

--- src/firestarter/X86/Payload/SSE2Payload.cpp
+++ src/firestarter/X86/Payload/SSE2Payload.cpp
@@ -39,8 +39,6 @@ auto SSE2Payload::compilePayload(const firestarter::payload::PayloadSettings& Se
                                  bool ErrorDetection, bool PrintAssembler,
                                  firestarter::payload::HighLoadControlFlowDescription ControlFlow) const
     -> firestarter::payload::CompiledPayload::UniquePtr {
-  (void)ControlFlow;
-
   using Imm = asmjit::Imm;
   using Mm = asmjit::x86::Mm;
   using Xmm = asmjit::x86::Xmm;
@@ -114,7 +112,11 @@ auto SSE2Payload::compilePayload(const firestarter::payload::PayloadSettings& Se
   const auto TempReg2 = asmjit::x86::rbp;
   const auto OffsetReg = asmjit::x86::r12;
   const auto AddrHighReg = asmjit::x86::r13;
+  // This register contains the current number of loop iterations
   const auto IterReg = asmjit::x86::r14;
+  // This register holds the remaining number of iterations, if the payload is compiled with
+  // HighLoadControlFlowDescription::kMaxIterationCount
+  const auto RemainingIterationsReg = asmjit::x86::r15;
   constexpr const auto MovRegs = 0;
   const auto AddRegs = 14;
   const auto TransRegs = 2;
@@ -137,10 +139,10 @@ auto SSE2Payload::compilePayload(const firestarter::payload::PayloadSettings& Se
   }
   // make all other used registers dirty except RAX
   Frame.addDirtyRegs(L1Addr, L2Addr, L3Addr, RamAddr, L2CountReg, L3CountReg, RamCountReg, TempReg, TempReg2, OffsetReg,
-                     AddrHighReg, IterReg);
+                     AddrHighReg, IterReg, RemainingIterationsReg);
 
   asmjit::FuncArgsAssignment Args(&Func);
-  Args.assignAll(PointerReg, AddrHighReg, IterReg);
+  Args.assignAll(PointerReg, AddrHighReg, RemainingIterationsReg);
   Args.updateFuncFrame(Frame);
   Frame.finalize();
 
@@ -150,6 +152,16 @@ auto SSE2Payload::compilePayload(const firestarter::payload::PayloadSettings& Se
   // stop right away if low load is selected
   auto FunctionExit = Cb.newLabel();
 
+  // Set inital iteration count to zero
+  Cb.mov(IterReg, Imm(0));
+
+  if (ControlFlow == firestarter::payload::HighLoadControlFlowDescription::kMaxIterationCount) {
+    // return if remaining iteration count is zero
+    Cb.test(RemainingIterationsReg, RemainingIterationsReg);
+    Cb.jz(FunctionExit);
+  }
+
+  // stop right away if low load is selected
   Cb.mov(TempReg, ptr_64(AddrHighReg));
   Cb.test(TempReg, TempReg);
   Cb.jz(FunctionExit);
@@ -379,7 +391,17 @@ auto SSE2Payload::compilePayload(const firestarter::payload::PayloadSettings& Se
     // adds always two instruction
     Stats.Instructions += 2;
   }
-  Cb.inc(IterReg); // increment iteration counter
+
+  // increment iteration counter
+  Cb.inc(IterReg);
+  if (ControlFlow == firestarter::payload::HighLoadControlFlowDescription::kMaxIterationCount) {
+    // decrement remaining iterations
+    Cb.dec(RemainingIterationsReg);
+    // Return if the remaining instructions reached zero
+    Cb.test(RemainingIterationsReg, RemainingIterationsReg);
+    Cb.jz(FunctionExit);
+  }
+
   Cb.mov(L1Addr, PointerReg);
 
   if (DumpRegisters) {
